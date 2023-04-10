ee957e3d713c4193329117ef09de0c9170a9bcf0
hong <j50508@gmail.com>
2023-04-10 20:24:06 +0800

    Up test codes

--- others/thread/tls.py
+++ others/thread/tls.py
@@ -1,19 +1,28 @@
-import threading
-
 #Play with ChatGPT
 #請問Python程式語言有Thread-local storage的機制嗎?
 #是的，Python提供了Thread-local storage的機制，稱為threading.local。Thread-local storage是一個線程內部的變數，每個線程都有自己獨立的副本，可以在線程內部訪問，但在不同線程之間是互不可見的。
 
-mydata = threading.local()
-mydata.x = 1  # 將變量x賦值給local對象的屬性
+import threading
 
+# 創建一個本地變數對象
+local_data = threading.local()
+
+# 定義一個函數
 def func():
-    mydata.x += 1
-    print(mydata.x)
+    # 設置本地變數的值
+    local_data.value = 100
+    # 獲取本地變數的值
+    print(local_data.value)
 
+# 創建兩個執行緒
 t1 = threading.Thread(target=func)
 t2 = threading.Thread(target=func)
-t1.start()  # 執行線程t1
-t2.start()  # 執行線程t2
-t1.join()   # 等待線程t1執行完畢
-t2.join()   # 等待線程t2執行完畢
+
+# 啟動執行緒
+t1.start()
+t2.start()
+
+# 等待執行緒結束
+t1.join()
+t2.join()
+

f8cd148e8b7b050a1b81ceb8d680006d48433f6f
hong <j50508@gmail.com>
2023-04-12 23:07:15 +0800

    Up codes

---
+++ Arcade/Intro/shapeArea.py
@@ -0,0 +1,10 @@
+
+def solution(n):
+    return (1 + (n-1)*n) *2 - 1
+
+if __name__ == "__main__":
+    print(solution(1))
+    print(solution(2))
+    print(solution(3))
+    print(solution(4))
+    print(solution(5))

c325b0258e999f046f9b0115f01d5e00259befe1
hong <j50508@gmail.com>
2023-04-16 08:41:13 +0800

    Up codes

---
+++ others/built-in-functions/test_dir.py
@@ -0,0 +1,5 @@
+#reference:https://docs.python.org/3.8/library/functions.html?highlight=dir#dir
+#reference:https://www.geeksforgeeks.org/python-dir-function/
+import array as arr
+
+print(dir(arr.typecodes))

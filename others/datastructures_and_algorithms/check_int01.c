f3cb0dac7ca6d4a1e22483bf5cc51e2eed36c95b
hong <j50508@gmail.com>
2023-04-26 12:26:28 +0800

    Up codes

--- others/datastructures_and_algorithms/check_int01.c
+++ others/datastructures_and_algorithms/check_int01.c
@@ -7,4 +7,5 @@ int32_t abs(int32_t x) {
 // 取絕對值
 // abs(-2147483648)?
 // 解讀計算機編碼
+// constant time?
 // src: https://hackmd.io/@sysprog/binary-representation#%E6%95%B8%E5%80%BC%E8%A1%A8%E7%A4%BA%E7%9A%84%E6%9C%89%E6%95%88%E7%AF%84%E5%9C%8D

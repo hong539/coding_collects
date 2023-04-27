e12b8347cefeee0adcfce669fd9bb4aeedf00615
hong <j50508@gmail.com>
2023-04-27 14:04:31 +0800

    Up codes

---
+++ others/basic_data_types/check_sizeof01.cpp
@@ -0,0 +1,17 @@
+// C++ Program to Demonstrate the correct size
+// of various data types on your computer.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Size of char : " << sizeof(char) << endl;
+	cout << "Size of int : " << sizeof(int) << endl;
+
+	cout << "Size of long : " << sizeof(long) << endl;
+	cout << "Size of float : " << sizeof(float) << endl;
+
+	cout << "Size of double : " << sizeof(double) << endl;
+
+	return 0;
+}

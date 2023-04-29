02945336d57c0ee158f24266b5c33fd743807f71
hong <j50508@gmail.com>
2023-04-29 19:29:53 +0800

    Up codes

---
+++ basic/list_stacks01.py
@@ -0,0 +1,10 @@
+stack = ["task01", "task02", "task03"]
+print("now:", stack)
+stack.append("task04")
+print("now:", stack)
+stack.append("task05")
+print("now:", stack)
+stack.pop()
+print("now:", stack)
+stack.pop()
+print("now:", stack)

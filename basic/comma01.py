66c9f3f46e8a29018d5e37c321bd1ae826b6ea68
hong <j50508@gmail.com>
2023-05-02 21:58:32 +0800

    Up codes

---
+++ basic/comma01.py
@@ -0,0 +1,25 @@
+# "," meaning what in Python?
+#https://docs.python.org/3.8/faq/programming.html?highlight=comma#what-s-up-with-the-comma-operator-s-precedence
+
+# 函數的參數列表中使用逗號","分隔多個參數
+def my_function(arg1, arg2, arg3):
+    # 函數的內容
+
+# 列表中使用逗號分隔不同的元素
+my_list = [1, 2, 3, 4, 5]
+
+# 元組中使用逗號分隔不同的元素
+my_tuple = (1, 2, 3, 4, 5)
+
+# 集合中使用逗號分隔不同的元素
+my_set = {1, 2, 3, 4, 5}
+
+
+# 多重賦值
+a, b, c = 1, 2, 3
+
+# 使用逗號分隔元素，括號可以省略
+my_tuple = 1, 2, 3
+
+# 輸出多個項目
+print("Hello", "world")

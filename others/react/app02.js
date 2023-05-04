86777129723f70aa3b6eb2a9e3f38f18efc9fb35
hong <j50508@gmail.com>
2023-05-04 17:21:15 +0800

    Up codes

---
+++ others/react/app02.js
@@ -0,0 +1,18 @@
+export default function TodoList() {
+    return (
+      <div>
+        <h1>Hedy Lamarr's Todos</h1>
+        <img 
+          src="https://i.imgur.com/yXOvdOSs.jpg" 
+          alt="Hedy Lamarr" 
+          className="photo" 
+        />
+        <ul>
+          <li>Invent new traffic lights</li>
+          <li>Rehearse a movie scene</li>
+          <li>Improve the spectrum technology</li>
+        </ul>
+      </div>
+    );
+  }
+  

dda838af4f4ae0629e3df11af6e14b0f5bcaadf6
hong <j50508@gmail.com>
2023-05-06 20:30:17 +0800

    Up codes

---
+++ basic/conditions01.py
@@ -0,0 +1,4 @@
+poor_guy01_cash = 50
+rich_guy01_cash = 100
+
+print(poor_guy01_cash, "equals", rich_guy01_cash) if poor_guy01_cash == rich_guy01_cash else print(poor_guy01_cash, "not equals", rich_guy01_cash)
---
+++ basic/if_else01.py
@@ -0,0 +1,9 @@
+poor_guy01_cash = 50
+rich_guy01_cash = 100
+
+if poor_guy01_cash == rich_guy01_cash:
+    print(poor_guy01_cash, "is equals:", rich_guy01_cash)
+elif poor_guy01_cash > rich_guy01_cash:
+    print(poor_guy01_cash, "is greater than", rich_guy01_cash)
+else:
+    print(poor_guy01_cash, "is less than", rich_guy01_cash)

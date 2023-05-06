2e4e135adfc72d7a950e17f52cb51fc9f1b40034
hong <j50508@gmail.com>
2023-05-06 20:42:55 +0800

    Up codes

--- basic/functions01.py
+++ basic/functions01.py
@@ -11,4 +11,9 @@ my_function02("This is 1st argument ")
 def my_function03(arg01, arg02):
     print("In my_function03 \n arg01:\n", arg01 , "\n arg02:\n", arg02)
     
-my_function03("1st", "2nd")    
+my_function03("1st", "2nd")
+
+def my_function04(*args):
+    print("Ths arg at index 0 is:", args[0])
+
+my_function04("some one")    

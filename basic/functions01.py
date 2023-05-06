2c19dee0b3710947fbb153b62c8946c5479f5530
hong <j50508@gmail.com>
2023-05-06 20:39:36 +0800

    Up codes

---
+++ basic/functions01.py
@@ -0,0 +1,14 @@
+def my_function():
+    print("my_function")
+
+my_function()
+
+def my_function02(argument01):
+    print("my_function", argument01)
+    
+my_function02("This is 1st argument ")
+    
+def my_function03(arg01, arg02):
+    print("In my_function03 \n arg01:\n", arg01 , "\n arg02:\n", arg02)
+    
+my_function03("1st", "2nd")    

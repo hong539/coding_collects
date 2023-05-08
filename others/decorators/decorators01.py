2f8deb70c021a86686a8599a11458c6b2909891f
hong <j50508@gmail.com>
2023-05-08 23:00:26 +0800

    Up codes

--- others/decorators/decorators01.py
+++ others/decorators/decorators01.py
@@ -1,17 +1,23 @@
 #concepts for python decorators
+#src: https://youtu.be/FXUUSfJO_J4
+import functools
 
 def start_end_decorator(func):
     
-    def wrapper():
+    @functools.wraps(func)
+    def wrapper(*args, ** kwargs):
         print("Start")
-        func()
+        result = func(*args, ** kwargs)        
         print("End")
+        return result
     return wrapper
 
 @start_end_decorator
-def print_name():
-    print("Joe")
+def add5(x):
+    return x + 5
 
 # print_name = start_end_decorator(print_name)
 
-print_name()
+result = add5(10)
+# print(help(add5))
+print(add5.__name__)

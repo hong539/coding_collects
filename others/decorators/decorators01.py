e4d26ec15d74a65284e80717a196eed4ff3f9ec1
hong <j50508@gmail.com>
2023-05-08 22:47:48 +0800

    Up codes

--- others/decorators/decorators01.py
+++ others/decorators/decorators01.py
@@ -1,5 +1,17 @@
-def plus_one(number):
-    return number + 1
+#concepts for python decorators
 
-add_one = plus_one
-print(add_one(5))
+def start_end_decorator(func):
+    
+    def wrapper():
+        print("Start")
+        func()
+        print("End")
+    return wrapper
+
+@start_end_decorator
+def print_name():
+    print("Joe")
+
+# print_name = start_end_decorator(print_name)
+
+print_name()

8337c363af936fad4f7b9062b488b8e2dd74fee6
hong <j50508@gmail.com>
2023-05-11 20:51:02 +0800

    Up codes

--- others/datastructures_and_algorithms/series01.py
+++ others/datastructures_and_algorithms/series01.py
@@ -4,10 +4,15 @@
 #And if
 # (7*7) + (9*9) + (11*11) + ... + (97 * 97) = ?
 
-def square(num) :
-    sum = 0
-    for i in range(1, num+1) :
-        sum= sum + (i * i)
-    return sum
-num = 6
-print("Sum of square is:",square(num))
+#method 1
+#for loop
+total = 0
+for i in range(7, 98, 2):
+    total += pow(i, 2)
+print(total)
+
+#metgod 2
+#with math calculations
+def sum(n):
+    return (n+1)*(4*n**2+44*n+147)//3
+print (sum(45))

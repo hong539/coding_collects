1eb13190b67acfc161a5ef865984709d29b383f8
hong <j50508@gmail.com>
2023-05-11 17:54:07 +0800

    Up codes

--- others/datastructures_and_algorithms/series01.py
+++ others/datastructures_and_algorithms/series01.py
@@ -1,7 +1,13 @@
 # series
+#Fisrt consider this series
+# (1*1) + (2*2) + (3*3) + ... +(100 * 100) =?
+#And if
 # (7*7) + (9*9) + (11*11) + ... + (97 * 97) = ?
 
-sum = 0
-for i in range(7, 98, 2):
-    sum += i * i
-print(sum)
+def square(num) :
+    sum = 0
+    for i in range(1, num+1) :
+        sum= sum + (i * i)
+    return sum
+num = 6
+print("Sum of square is:",square(num))

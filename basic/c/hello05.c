7809564820c687dd9b6ef3ee25bb1c04862c2f5e
hong <j50508@gmail.com>
2023-05-12 11:35:36 +0800

    Up codes

--- basic/c/hello05.c
+++ basic/c/hello05.c
@@ -11,3 +11,4 @@ int main() {
 
 //readable?
 //execution times?
+//after compile?
---
+++ basic/c/hello07.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("H     H     A\n"
+           "H     H    A A\n"
+           "HHHHHHH   A   A\n"
+           "H     H  AAAAAAA\n"
+           "H     H A       A\n");
+    return 0;
+}
+
+//readable?
+//execution times?
+//after compile?

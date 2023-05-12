d9d4f5a3c4f74b5315ce63082ecb488891f5e9c9
hong <j50508@gmail.com>
2023-05-12 11:15:33 +0800

    Up codes

---
+++ basic/c/README.md
@@ -0,0 +1,7 @@
+# c
+
+## how to compile codes with tools
+
+```shell
+gcc -o bin_name codes.c
+```
---
+++ basic/c/hello04.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello \\n world!");
+    return 0;
+}
+
+// escape
+// \n
+// \"
+// \\
+// \'
+// \t

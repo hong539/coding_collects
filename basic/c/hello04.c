4566e69fa905a2b0f13a1fe316c89438019bbec1
hong <j50508@gmail.com>
2023-05-12 11:19:09 +0800

    Up codes

--- basic/c/hello04.c
+++ basic/c/hello04.c
@@ -5,6 +5,7 @@ int main() {
     return 0;
 }
 
+// 常見的跳脫(轉義)序列
 // escape
 // \n
 // \"
---
+++ basic/c/hello05.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("H     H\n");
+    printf("H     H\n");
+    printf("HHHHHHH\n");
+    printf("H     H\n");
+    printf("H     H\n");
+    return 0;
+}

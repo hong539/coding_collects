21d6bb5681f1da76689445393dac13d6996ead6f
hong <j50508@gmail.com>
2023-05-12 11:25:00 +0800

    Up codes

--- basic/c/hello05.c
+++ basic/c/hello05.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("H     H\n");
-    printf("H     H\n");
-    printf("HHHHHHH\n");
-    printf("H     H\n");
-    printf("H     H\n");
+    printf("H     H     A\n");
+    printf("H     H    A A\n");
+    printf("HHHHHHH   A   A\n");
+    printf("H     H  AAAAAAA\n");
+    printf("H     H A       A\n");
     return 0;
 }

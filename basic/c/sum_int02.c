ff7609eb0304f3b0f28d9c244799b24812424094
hong <j50508@gmail.com>
2023-05-13 12:28:51 +0800

    Up codes

--- basic/c/sum_int02.c
+++ basic/c/sum_int02.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int integer1, integer2, sum;
+    int integer1, integer2, integer3, sum;
     printf("Please enter the first interger: ");
     scanf("%d", &integer1);
     printf("Please enter the second interger: ");
     scanf("%d", &integer2);
-    sum = integer1 + integer2;
+    printf("Please enter the third interger: ");
+    scanf("%d", &integer3);    
+    sum = integer1 + integer2 + integer3;
     printf("Sum is %d. \n", sum);
     return 0;
 }

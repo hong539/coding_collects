7596bb8b207c0aa2c7dcb136e4ee368fba45affb
hong <j50508@gmail.com>
2023-05-13 13:26:59 +0800

    Up c basic codes

---
+++ basic/c/exchange_int02.c
@@ -0,0 +1,20 @@
+// integer1, integer2 -> 2, 6
+// after exchange integer1, integer2 -> 6, 2
+// C語言無法多重指定
+#include <stdio.h>
+
+int main() {
+    int integer1, integer2;
+    printf("Please enter the first interger: ");
+    scanf("%d", &integer1);
+    printf("Please enter the second interger: ");
+    scanf("%d", &integer2);
+
+    integer1 = integer1 + integer2;
+    integer2 = integer1 - integer2;
+    integer1 = integer1 - integer2;
+    
+    printf("integer1: %d\n", integer1);
+    printf("integer2: %d\n", integer2);
+    return 0;
+}

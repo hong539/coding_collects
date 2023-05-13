e56607173cfb6e9fba5e5e2f8ccc08c62fc803a5
hong <j50508@gmail.com>
2023-05-13 13:29:43 +0800

    Up c basic codes

--- basic/c/exchange_int03.c
+++ basic/c/exchange_int03.c
@@ -1,7 +1,7 @@
 // integer1, integer2 -> 2, 6
 // after exchange integer1, integer2 -> 6, 2
 // C語言無法多重指定
-// 整數的特性(可以做加/減)
+// 整數的特性
 #include <stdio.h>
 
 int main() {
@@ -11,9 +11,9 @@ int main() {
     printf("Please enter the second interger: ");
     scanf("%d", &integer2);
 
-    integer1 = integer1 + integer2;
-    integer2 = integer1 - integer2;
-    integer1 = integer1 - integer2;
+    integer1 = integer1 ^ integer2;
+    integer2 = integer1 ^ integer2;
+    integer1 = integer1 ^ integer2;
     
     printf("integer1: %d\n", integer1);
     printf("integer2: %d\n", integer2);

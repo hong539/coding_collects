0783c10b9578b302db5d499ce1501be6cfccd355
hong <j50508@gmail.com>
2023-05-16 23:43:41 +0800

    Up codes

---
+++ codesignal/Arcade/Intro/all_longest_strings.py
@@ -0,0 +1,8 @@
+def solution(inputArray:list) -> list:
+    for l in inputArray:
+        pass
+    return len(inputArray[0])
+
+if __name__ == "__main__":
+    inputArray = ["aba", "aa", "ad", "vcd", "aba"]
+    print(solution(inputArray))

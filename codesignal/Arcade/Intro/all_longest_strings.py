5c93a9974a529dc9c96bb2799a78a6bd050c0082
hong <j50508@gmail.com>
2023-05-17 01:18:08 +0800

    Test codes

--- codesignal/Arcade/Intro/all_longest_strings.py
+++ codesignal/Arcade/Intro/all_longest_strings.py
@@ -5,20 +5,21 @@ def solution(inputArray:list) -> list:
     #TypeError: can only concatenate str (not "int") to str
     #ValueError: invalid literal for int() with base 10: 'aba'
     result = []
-    # temp = len(max(inputArray, key=len))
-    # # print(temp)
-    # for l in inputArray:
-    #     if len(l) == temp:
-    #         result.append(l)
-    for i in range(0, len(inputArray)-1):
-        item1 = inputArray[i]
-        item2 = inputArray[i+1]
+    temp = len(max(inputArray, key=len))
+    # print(temp)
+    for l in inputArray:
+        if len(l) == temp:
+            result.append(l)
+    # for i in range(0, len(inputArray)-1):
+    #     item1 = inputArray[i]
+    #     item2 = inputArray[i+1]
         
-        if len(item1) == len(item2):
-            result.append(item1)
+    #     if len(item1) == len(item2):
+    #         result.append(item1)
     
     return result
 
 if __name__ == "__main__":
     inputArray = ["aba", "aa", "ad", "vcd", "aba"]
     print(solution(inputArray))
+    #output = ["aba", "vcd", "aba"]

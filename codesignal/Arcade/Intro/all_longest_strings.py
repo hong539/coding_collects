232996ae7cb271142d9a88b98df73baa30a5ab6c
hong <j50508@gmail.com>
2023-05-17 00:13:29 +0800

    Up codes

--- codesignal/Arcade/Intro/all_longest_strings.py
+++ codesignal/Arcade/Intro/all_longest_strings.py
@@ -1,7 +1,17 @@
 def solution(inputArray:list) -> list:
+    #check string length in list
+    #creat new list from longest strings
+    #convert str to int
+    #ValueError: invalid literal for int() with base 10: 'aba'
+    result = []
     for l in inputArray:
-        pass
-    return len(inputArray[0])
+        # print(type(l))
+        if len(int(l)) >= len(int(l+1)):
+            result.append(l)
+        else:
+            break
+    # print(result)
+    return result
 
 if __name__ == "__main__":
     inputArray = ["aba", "aa", "ad", "vcd", "aba"]

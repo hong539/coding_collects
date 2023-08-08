11ff534156a426873d23ca4b443cd3355bfa6627
hong@arch-pyton-dev <yunghunghuang984@gmail.com>
2023-08-09 03:37:28 +0800

    Update codes

--- basic/python/open01.py
+++ basic/python/open01.py
@@ -1,3 +1,5 @@
-with open('./00:10:23.1/logfile', 'r') as f:
+# Why cant I create a directory in /sys
+# https://askubuntu.com/questions/341939/why-cant-i-create-a-directory-in-sys
+with open('./sys/debug/logfile', 'r') as f:
     content = f.read()
     print(content)

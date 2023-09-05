ae91c17e0f0703ea5cfe7deb65afe8b5e74ca8fa
hong@arch-pyton-dev <yunghunghuang984@gmail.com>
2023-09-05 16:29:27 +0800

    Update codes

--- others/Genshin Impact/calculator.py
+++ others/Genshin Impact/calculator.py
@@ -13,41 +13,30 @@
 #名刀鐔 = 影打刀鐔*3 = (破舊的刀鐔*3)*3 = 破舊的刀鐔*9
 
 class Item:
-    def __init__(self, name, value) -> dict():
+    def __init__(self, name, value) -> None:
         self.name = name
         self.value = value
-        return {self.name:self.value}
+        # self.dict = {}
+        # self.dict[self.name] = self.value        
 
 class Inventory:
-    def __init__(self) -> dict():
-        self.items = dict()
-        return self.items
+    def __init__(self) -> None:
+        self.items = {}        
         
     def add_item(self, item):
-        self.items.update(item)
+        self.items.update({item.name: item.value})
         
     def remove_item(self, item):
         self.items.remove(item)
 
     def display_inventory(self):
-        for item in self.items:
-            print(item.name + ": " + item.value)
+        for item in self.items.items():
+            print(item)
         
 
 player_inventory01 = Inventory()
+# print(type(Item("破舊的刀鐔", 202)))
 player_inventory01.add_item(Item("破舊的刀鐔", 202))
 player_inventory01.add_item(Item("影打刀鐔", 64))
 player_inventory01.add_item(Item("名刀鐔", 9))
 player_inventory01.display_inventory()
-
-target_items = {"破舊的刀鐔": 36,
-                "影打刀鐔": 96,
-                "名刀鐔": 66}
-
-currnet_items = {"破舊的刀鐔": 202,
-                "影打刀鐔": 64,
-                "名刀鐔": 9}
-
-currnet_items_all_to_base = currnet_items["破舊的刀鐔"] + currnet_items["影打刀鐔"]*3 + (currnet_items["名刀鐔"]*3)*3
-
-print(currnet_items_all_to_base)

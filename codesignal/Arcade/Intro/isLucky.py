326a8fb36b8e1a91e4636cd9e3c937301c149edf
hong@arch-pyton-dev <yunghunghuang984@gmail.com>
2023-10-16 22:19:22 +0800

    Update codes

--- codesignal/Arcade/Intro/isLucky.py
+++ codesignal/Arcade/Intro/isLucky.py
@@ -9,3 +9,11 @@ def solution(n):
 
 if __name__ == "__main__":
     solution(1230)
+    solution(239017)
+    solution(134008)
+    solution(10)
+    solution(1010)
+    solution(261534)
+    solution(100000)
+    solution(999999)
+    solution(123321)

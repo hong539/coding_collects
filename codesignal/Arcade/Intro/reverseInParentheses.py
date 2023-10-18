048b1ef79d63247e0e0f68fea8d17c97738d29a2
hong@arch-pyton-dev <yunghunghuang984@gmail.com>
2023-10-18 12:55:15 +0800

    Test codes

--- codesignal/Arcade/Intro/reverseInParentheses.py
+++ codesignal/Arcade/Intro/reverseInParentheses.py
@@ -5,12 +5,18 @@ def solution(inputString:str)->str:
     # return result
     print("inputString is:", inputString)
     temp = []
+    final = []
     for x in inputString:
         if x not in ("(", ")"):
             temp.append(x)
         else:
             continue
     print(temp)
+    print(temp[::-1])
+    ss = final.extend(temp)
+    print(ss)
+    # return "".join(temp)
+    # return "".join(temp[::-1])    
     # temp = inputString.strip("(").strip(")")
     # reverse = temp[::-1]
     # print(reverse)
@@ -19,3 +25,8 @@ def solution(inputString:str)->str:
 if __name__ == "__main__":
     solution("(bar)")
     solution("foo(bar)baz")
+    # solution("foo(bar)baz(blim)")
+    # solution("foo(bar(baz))blim")
+    # solution("")
+    # solution("()")
+    # solution("(abc)d(efg)")

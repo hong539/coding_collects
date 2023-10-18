06e6833cbe52d817e09023bea7c1bf4c0b664842
hong@arch-pyton-dev <yunghunghuang984@gmail.com>
2023-10-18 14:47:41 +0800

    Test codes

--- codesignal/Arcade/Intro/reverseInParentheses.py
+++ codesignal/Arcade/Intro/reverseInParentheses.py
@@ -3,36 +3,17 @@ def solution(inputString:str)->str:
     # reverse temp string
     # combine temp to result
     # return result
-    print("inputString is:", inputString)
-    seen_even = True
-    temp = []
-    result = []
-    for x in inputString:
-        # print("for loop here temp is:", temp)
-        if x not in ('(', ')'):
-            temp.append(x)            
-        else:
-            if seen_even:
-                result.extend(temp)
-            else:
-                result.extend(temp[::-1])
-            seen_even = not seen_even
-            temp = []
-
-    if temp:
-        if seen_even:
-            result.extend(temp)            
-        else:
-            result.extend(temp[::-1])            
-    final = ''.join(result)
-    print(final)
-    return final
+    temp = inputString.strip('(').strip(')')
+    reverse = temp[::-1]
+    print(reverse)
+    
+    return reverse
 
 if __name__ == "__main__":
-    # solution("(bar)")
+    solution("(bar)")
     # solution("foo(bar)baz")
     # solution("foo(bar)baz(blim)")
-    solution("foo(bar(baz))blim")
+    # solution("foo(bar(baz))blim")
     # solution("")
     # solution("()")
     # solution("(abc)d(efg)")

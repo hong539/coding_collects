5848f5a5b37e4c3f3c69ebed724c739f4309ea86
Hong <hong@ubuntu-lab-01>
2025-02-22 14:22:08 +0800

    Add exercism/go/         party-robot/         welcome-to-tech-palace/

--- exercism/go/cars-assemble/cars_assemble.go
+++ exercism/go/cars-assemble/cars_assemble.go
@@ -1,18 +1,23 @@
 package cars
 
-// CalculateWorkingCarsPerHour calculates how many working cars are
-// produced by the assembly line every hour.
+// CalculateWorkingCarsPerHour calculates how many working cars are produced by the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
-	panic("CalculateWorkingCarsPerHour not implemented")
+	return float64(productionRate) * (successRate / 100.0)
 }
 
-// CalculateWorkingCarsPerMinute calculates how many working cars are
-// produced by the assembly line every minute.
+// CalculateWorkingCarsPerMinute calculates how many working cars are produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	panic("CalculateWorkingCarsPerMinute not implemented")
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	panic("CalculateCost not implemented")
+	const bulkSize = 10
+	const bulkPrice = 95000 // 10 cars for $95,000
+	const singleCarPrice = 10000
+
+	bulkCars := carsCount / bulkSize
+	remainingCars := carsCount % bulkSize
+
+	return uint(bulkCars*bulkPrice + remainingCars*singleCarPrice)
 }
